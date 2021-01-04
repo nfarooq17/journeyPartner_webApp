7c12f551e575791c7c00987e14476b419cc315d6
nfarooq17 <62171733+nfarooq17@users.noreply.github.com>
2021-01-04 18:32:18 +0500

    Add files via upload

---
+++ build/precache-manifest.78f8feef7f1ce516d3fa4ea8bb45d478.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2c158703b66af75a58bc8781394c154",
+    "url": "/Journey-Partner/index.html"
+  },
+  {
+    "revision": "67b6b6acf2c0c8eb0f06",
+    "url": "/Journey-Partner/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f21e0e4655946fa3efd0",
+    "url": "/Journey-Partner/static/js/2.8ea2b59b.chunk.js"
+  },
+  {
+    "revision": "236fe0529f16d4e7f52ab6b59e395f26",
+    "url": "/Journey-Partner/static/js/2.8ea2b59b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67b6b6acf2c0c8eb0f06",
+    "url": "/Journey-Partner/static/js/main.59a29831.chunk.js"
+  },
+  {
+    "revision": "f07969ee2ad812edf154",
+    "url": "/Journey-Partner/static/js/runtime-main.5feb426b.js"
+  }
+]);
